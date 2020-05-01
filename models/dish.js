49dc3d1d70406df47abf5e5722e973968e88370c
daikon0 <hamada1207.daiki@gmail.com>
2020-05-01 11:52:07 +0900

    add sequelize

---
+++ models/dish.js
@@ -0,0 +1,24 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const dish = sequelize.define(
+    'dish',
+    {
+      dishId: {
+        type: DataTypes.UUID,
+        primaryKey: true
+      },
+      dishname: DataTypes.STRING,
+      dishFile: DataTypes.STRING,
+      dishUrl: DataTypes.STRING,
+      dishGenre: DataTypes.STRING,
+      dishRole: DataTypes.STRING,
+      createdBy: DataTypes.BIGINT,
+      updatedAt: DataTypes.DATE
+    },
+    {}
+  )
+  dish.associate = function(models) {
+    // associations can be defined here
+  }
+  return dish
+}
