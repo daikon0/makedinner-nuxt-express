1292c10d3f14b3ba90cd8721f85ebf953ed30240
daikon0 <hamada1207.daiki@gmail.com>
2020-05-04 09:27:17 +0900

    delete log

---
+++ routes/menu.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../models/index')
+
+router.get('/mypage/menu', async (req, res, next) => {
+  const dish = await db.dish.findAll({
+    where: { createdBy: req.user.name.id },
+    order: [['"updatedAt"', 'DESC']]
+  })
+  res.send(dish)
+})
+
+module.exports = router
