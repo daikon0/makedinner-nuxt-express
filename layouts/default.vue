d889cb36640930f3786ba60eda461a86e77400ce
daikon0 <hamada1207.daiki@gmail.com>
2020-05-08 16:48:34 +0900

    change color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,48 +1,51 @@
 <template>
-  <v-card class="mx-auto overflow-hidden" height="1000">
-    <v-app-bar color="deep-purple">
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+  <v-app>
+    <v-card class="overflow-hidden" height="1000">
+      <v-app-bar color="info">
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Make Dinner</v-toolbar-title>
-    </v-app-bar>
-    <nuxt />
+        <v-toolbar-title>Make Dinner</v-toolbar-title>
+      </v-app-bar>
+      <nuxt />
 
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item to="/mypage">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item to="/mypage">
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
 
-          <v-list-item to="/mypage/menu">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Menu</v-list-item-title>
-          </v-list-item>
+            <v-list-item to="/mypage/menu">
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Menu</v-list-item-title>
+            </v-list-item>
 
-          <v-list-item to="/mypage/selectGenre">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>MakeDinner</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+            <v-list-item to="/mypage/selectGenre">
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>MakeDinner</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
-    drawer: false
+    drawer: false,
+    group: ''
   })
 }
 </script>
--- layouts/signout.vue
+++ layouts/signout.vue
@@ -1,48 +1,51 @@
 <template>
-  <v-card class="mx-auto overflow-hidden" height="1000">
-    <v-app-bar color="deep-purple">
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+  <v-app>
+    <v-card class="overflow-hidden" height="1000">
+      <v-app-bar color="info" dark>
+        <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Make Dinner</v-toolbar-title>
-    </v-app-bar>
-    <nuxt />
+        <v-toolbar-title>Make Dinner</v-toolbar-title>
+      </v-app-bar>
+      <nuxt />
 
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="deep-purple--text text--accent-4"
-        >
-          <v-list-item to="/">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <v-list nav dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4"
+          >
+            <v-list-item to="/">
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
 
-          <v-list-item to="/signin">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Sign In</v-list-item-title>
-          </v-list-item>
+            <v-list-item to="/signin">
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Sign In</v-list-item-title>
+            </v-list-item>
 
-          <v-list-item to="/register">
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Register</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </v-card>
+            <v-list-item to="/register">
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Register</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </v-card>
+  </v-app>
 </template>
 
 <script>
 export default {
   data: () => ({
-    drawer: false
+    drawer: false,
+    group: ''
   })
 }
 </script>
