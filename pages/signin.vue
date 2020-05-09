8155eb45f794e78fa603b96510d15e6d306cb395
daikon0 <hamada1207.daiki@gmail.com>
2020-05-09 15:02:54 +0900

    change responsive

--- pages/signin.vue
+++ pages/signin.vue
@@ -1,39 +1,46 @@
 <template>
   <v-app>
-    <v-card>
-      <div>
-        <v-card-title>Sign In</v-card-title>
-        <v-card-text>
-          <v-form ref="test_form">
-            <v-text-field
-              v-model="username"
-              label="usernmae"
-              :rules="[required]"
-            ></v-text-field>
-            <v-text-field
-              v-model="password"
-              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-              :type="show1 ? 'text' : 'password'"
-              label="password"
-              :rules="[required, limit_length]"
-              counter="8"
-              @click:append="show1 = !show1"
-            ></v-text-field>
-          </v-form>
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-btn
-            :disabled="!username || !(password.length >= 8)"
-            text
-            @click="submit"
-          >
-            送信する
-          </v-btn>
-          <span v-if="success">success!</span>
-        </v-card-actions>
-      </div>
-    </v-card>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" md="8">
+          <v-card class="mt-5">
+            <div>
+              <v-row justify="center">
+                <v-col cols="12" md="8">
+                  <v-card-title>Sign In</v-card-title>
+                  <v-card-text>
+                    <v-form ref="test_form">
+                      <v-text-field
+                        v-model="username"
+                        label="usernmae"
+                        :rules="[required]"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="password"
+                        :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+                        :type="show1 ? 'text' : 'password'"
+                        label="password"
+                        :rules="[required, limit_length]"
+                        counter="8"
+                        @click:append="show1 = !show1"
+                      ></v-text-field>
+                      <v-btn
+                        :disabled="!username || !(password.length >= 8)"
+                        text
+                        class="my-5"
+                        @click="submit"
+                      >
+                        Sign In
+                      </v-btn>
+                    </v-form>
+                  </v-card-text>
+                </v-col>
+              </v-row>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
