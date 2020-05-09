4aac54db8c577bb073cd455fda543adb244ad9ea
daikon0 <hamada1207.daiki@gmail.com>
2020-05-09 16:51:17 +0900

    delete style

--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -24,32 +24,3 @@
     </div>
   </div>
 </template>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #506081;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
