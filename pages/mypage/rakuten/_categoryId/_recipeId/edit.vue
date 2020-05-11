7d5867a2557b54c32e08fb09645292cfb7fd3a1f
daikon0 <hamada1207.daiki@gmail.com>
2020-05-11 16:13:55 +0900

    change design

--- pages/mypage/rakuten/_categoryId/_recipeId/edit.vue
+++ pages/mypage/rakuten/_categoryId/_recipeId/edit.vue
@@ -1,68 +1,77 @@
 <template>
-  <div>
-    <div class="container">
-      <h1>料理を追加する</h1>
-      <form action="/routes/createDish" method="post" class="form-group">
-        <div>
-          <label>料理名</label>
-          <input type="text" name="dishName" :value="recipe.recipeTitle" />
-        </div>
-        <div>
-          <label>レシピURL</label>
-          <input type="text" name="dishUrl" :value="recipe.recipeUrl" />
-        </div>
-        <div>
-          <input type="hidden" name="dishFile" :value="recipe.foodImageUrl" />
-        </div>
-        <div>
-          <label>
-            <input
-              type="radio"
-              name="genre"
-              value="japan"
-              autocomplete="off"
-              checked
-            />
-            和食
-          </label>
-          <label>
-            <input type="radio" name="genre" autocomplete="off" value="china" />
-            中華
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="genre"
-              autocomplete="off"
-              value="western"
-            />
-            洋食
-          </label>
-        </div>
-        <div>
-          <label>
-            <input
-              type="radio"
-              name="role"
-              value="main"
-              autocomplete="off"
-              checked
-            />
-            主菜
-          </label>
-          <label>
-            <input type="radio" name="role" value="sub" autocomplete="off" />
-            副菜
-          </label>
-          <label>
-            <input type="radio" name="role" value="soup" autocomplete="off" />
-            汁物
-          </label>
-        </div>
-        <button type="submit">料理を登録</button>
-      </form>
-    </div>
-  </div>
+  <v-app>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" md="8">
+          <v-card class="mt-5">
+            <div>
+              <v-row justify="center">
+                <v-col>
+                  <v-card-title>料理を登録する</v-card-title>
+                  <v-card-text>
+                    <v-form ref="test_form">
+                      <v-text-field
+                        v-model="recipe.recipeTitle"
+                        label="料理名"
+                        :rules="[required]"
+                      ></v-text-field>
+                      <v-text-field
+                        v-model="recipe.recipeUrl"
+                        label="レシピのURL"
+                      ></v-text-field>
+                      <v-radio-group v-model="dishGenre" :rules="[reqired]" row>
+                        <v-radio label="和食" value="japan"></v-radio>
+                        <v-radio label="洋食" value="western"></v-radio>
+                        <v-radio label="中華" value="china"></v-radio>
+                      </v-radio-group>
+                      <v-radio-group v-model="dishRole" :rules="[required]" row>
+                        <v-radio label="主菜" value="main"></v-radio>
+                        <v-radio label="副菜" value="sub"></v-radio>
+                        <v-radio label="汁物" value="soup"></v-radio>
+                      </v-radio-group>
+                    </v-form>
+                    <form action="/routes/createDish/rakuten" method="post">
+                      <input
+                        type="hidden"
+                        name="dishName"
+                        :value="recipe.recipeTitle"
+                      />
+                      <input
+                        type="hidden"
+                        name="dishUrl"
+                        :value="recipe.recipeUrl"
+                      />
+                      <input
+                        type="hidden"
+                        name="dishFile"
+                        :value="recipe.foodImageUrl"
+                      />
+                      <input
+                        type="hidden"
+                        name="dishGenre"
+                        :value="dishGenre"
+                      />
+                      <input type="hidden" name="dishRole" :value="dishRole" />
+                      <v-btn
+                        :disabled="
+                          !recipe.recipeTitle || !dishGenre || !dishRole
+                        "
+                        text
+                        class="my-5"
+                        type="submit"
+                      >
+                        登録する
+                      </v-btn>
+                    </form>
+                  </v-card-text>
+                </v-col>
+              </v-row>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -79,7 +88,9 @@ export default {
       if (item.recipeId === recipeId) return true
     })
     return {
-      recipe: recipe[0]
+      recipe: recipe[0],
+      dishGenre: '',
+      dishRole: ''
     }
   }
 }
--- pages/mypage/rakuten/index.vue
+++ pages/mypage/rakuten/index.vue
@@ -1,20 +1,40 @@
 <template>
-  <div>
-    <div>
-      <ul>
-        <li v-for="category in categories" :key="category.categoryId">
-          <nuxt-link
-            :to="{
-              name: 'mypage-rakuten-categoryId',
-              params: { categoryId: category.categoryId }
-            }"
-          >
-            {{ category.categoryName }}
-          </nuxt-link>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <v-app>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" md="8">
+          <v-card class="mt-5">
+            <div>
+              <v-row justify="center">
+                <v-col>
+                  <v-list rounded subheader>
+                    <v-subheader inset>rakuten category</v-subheader>
+                    <v-list-item-group>
+                      <v-list-item
+                        v-for="category in categories"
+                        :key="category.categoryId"
+                        :to="{
+                          name: 'mypage-rakuten-categoryId',
+                          params: { categoryId: category.categoryId }
+                        }"
+                      >
+                        <v-list-item-content text-center>
+                          <v-list-item-title
+                            class="my-3 text-center title"
+                            v-text="category.categoryName"
+                          ></v-list-item-title>
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list-item-group>
+                  </v-list>
+                </v-col>
+              </v-row>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 //
