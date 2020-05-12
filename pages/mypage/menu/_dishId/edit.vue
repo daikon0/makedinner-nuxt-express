da711b0c36238eb1b4bd6445821306b29361480c
daikon0 <hamada1207.daiki@gmail.com>
2020-05-12 14:31:55 +0900

    change design

--- pages/mypage/menu/_dishId/edit.vue
+++ pages/mypage/menu/_dishId/edit.vue
@@ -1,28 +1,47 @@
 <template>
-  <div>
-    <div class="container">
-      <h1>料理を編集する</h1>
-      <form class="form-group" @submit.prevent="edit">
-        <div>
-          <label>料理名</label>
-          <input v-model="dishName" type="text" name="dishName" />
-        </div>
-        <div>
-          <label>レシピURL</label>
-          <input v-model="dishUrl" type="text" name="dishUrl" />
-        </div>
-        <button type="submit">料理を登録</button>
-      </form>
-    </div>
-  </div>
+  <v-app>
+    <v-container fluid>
+      <v-row justify="center">
+        <v-col cols="12" md="8">
+          <v-card class="mt-5">
+            <div class="container">
+              <v-card-title>料理を編集する</v-card-title>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    v-model="dish.dishName"
+                    label="料理名"
+                    :rules="[required]"
+                  ></v-text-field>
+                  <v-text-field
+                    v-model="dish.dishUrl"
+                    label="レシピのURL"
+                  ></v-text-field>
+                  <v-btn class="my-5" :disabled="!dish.dishName" @click="edit">
+                    変更する
+                  </v-btn>
+                </v-form>
+              </v-card-text>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 export default {
+  async asyncData({ app }) {
+    const dishId = app.context.params.dishId
+    const dish = await app.$axios.$get(`/routes/mypage/menu/${dishId}`)
+    return {
+      dish
+    }
+  },
   data() {
     return {
-      dishName: '',
-      dishUrl: ''
+      required: (value) => !!value || '必ず入力してください'
     }
   },
   methods: {
@@ -30,8 +49,8 @@ export default {
       const id = this.$store.app.context.params.dishId
       await this.$axios
         .$post(`/routes/mypage/menu/${id}/edit`, {
-          dishName: this.dishName,
-          dishUrl: this.dishUrl
+          dishName: this.dish.dishName,
+          dishUrl: this.dish.dishUrl
         })
         .then(() => {
           this.$router.push('/mypage/menu')
