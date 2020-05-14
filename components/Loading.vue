7d8d5915b038471216b596eb6c33e64ca8f000f0
daikon0 <hamada1207.daiki@gmail.com>
2020-05-14 11:10:01 +0900

    add loading

---
+++ components/Loading.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-row justify="center" align-content="center">
+      <div class="text-center">
+        <v-progress-circular
+          indeterminate
+          :size="70"
+          :width="7"
+          color="primary"
+        ></v-progress-circular>
+      </div>
+    </v-row>
+  </v-app>
+</template>
