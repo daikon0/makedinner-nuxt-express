724120709ef3d178eea27a2443da109e602294a0
daikon0 <hamada1207.daiki@gmail.com>
2020-05-18 14:12:01 +0900

    add retry test

---
+++ test/retry.spec.js
@@ -0,0 +1,35 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Vuetify from 'vuetify'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import cloneDeep from 'lodash.clonedeep'
+import RetryPage from '~/pages/retry'
+import store from '~/store/index'
+
+const localVue = createLocalVue()
+Vue.use(Vuetify)
+localVue.use(Vuex)
+localVue.use(Vuetify)
+localVue.use(VueRouter)
+
+describe('pages/retry.vue', () => {
+  let wrapper
+  let vuetify
+  let router
+
+  beforeEach(() => {
+    wrapper = null
+    vuetify = new Vuetify()
+    router = new VueRouter()
+    wrapper = mount(RetryPage, {
+      store: new Vuex.Store(cloneDeep(store)),
+      localVue,
+      vuetify,
+      router
+    })
+  })
+  test('aaaa', () => {
+    expect(wrapper.find('.v-btn').props().to).toBe('/')
+  })
+})
