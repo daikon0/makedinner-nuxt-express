5c942cb7dbd36c3b0b2c1a723847b9857b870367
daikon0 <hamada1207.daiki@gmail.com>
2020-05-20 20:14:05 +0900

    change design

--- pages/mypage/oneDish/index.vue
+++ pages/mypage/oneDish/index.vue
@@ -1,29 +1,29 @@
 <template>
   <v-app>
     <v-container>
+      <div>
+        <v-btn-toggle class="my-3 mt-8 btn-width">
+          <v-btn block @click="ricebtn">
+            丼もの
+          </v-btn>
+          <v-btn block @click="noodlesbtn">
+            麺類
+          </v-btn>
+        </v-btn-toggle>
+      </div>
+      <div>
+        <v-btn-toggle class="my-3 btn-width">
+          <v-btn block @click="lightbtn">
+            あっさり
+          </v-btn>
+          <v-btn block @click="heavybtn">
+            こってり
+          </v-btn>
+        </v-btn-toggle>
+      </div>
+      <v-btn class="my-5" @click="decision">決定</v-btn>
       <v-row justify="center" align="center">
-        <v-col cols="12" md="8">
-          <div>
-            <v-btn-toggle>
-              <v-btn @click="ricebtn">
-                丼もの
-              </v-btn>
-              <v-btn @click="noodlesbtn">
-                麺類
-              </v-btn>
-            </v-btn-toggle>
-          </div>
-          <div>
-            <v-btn-toggle>
-              <v-btn @click="lightbtn">
-                あっさり
-              </v-btn>
-              <v-btn @click="heavybtn">
-                こってり
-              </v-btn>
-            </v-btn-toggle>
-          </div>
-          <v-btn @click="decision">決定</v-btn>
+        <v-col cols="12" md="9">
           <div v-if="dish">
             <Dish :dish="dish" />
           </div>
@@ -49,22 +49,24 @@ export default {
       oyakodon: {
         dishName: '親子丼',
         dishFile: require('@/static/oyakodon.jpg'),
-        dishUrl: 'aaaaaa'
+        dishUrl: 'https://park.ajinomoto.co.jp/recipe/card/703024/'
       },
       butadon: {
         dishName: '豚丼',
         dishFile: require('@/static/butadon.jpeg'),
-        dishUrl: 'aaaa'
+        dishUrl: 'https://cookpad.com/recipe/1134767'
       },
       udon: {
         dishName: 'ぶっかけうどん',
         dishFile: require('@/static/udon.jpeg'),
-        dishUrl: 'aaaa'
+        dishUrl:
+          'https://www.kurashiru.com/recipes/61bf7bb5-24ba-4bec-aac3-170a2ce5382f'
       },
       yakisoba: {
         dishName: '焼きそば',
         dishFile: require('@/static/yakisoba.jpeg'),
-        dishUrl: 'aaaa'
+        dishUrl:
+          'https://www.kurashiru.com/recipes/01e45c0e-7961-4e7d-b332-1f819e8aa10f'
       }
     }
   },
@@ -108,6 +110,10 @@ export default {
 </script>
 
 <style>
+.btn-width {
+  width: 50%;
+}
+
 a {
   text-decoration: none;
 }
