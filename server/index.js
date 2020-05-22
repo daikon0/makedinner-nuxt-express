340faa66e609c456b46562b11a86e368dcdb4fa3
daikon0 <hamada1207.daiki@gmail.com>
2020-05-22 10:37:07 +0900

    add helmet

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const helmet = require('helmet')
+app.use(helmet())
 
 const session = require('express-session')
 const passport = require('passport')
