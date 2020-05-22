51dd75d7356b0b9f731379784fb0fee7ef75978c
daikon0 <hamada1207.daiki@gmail.com>
2020-05-22 19:02:32 +0900

    change space

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
     <v-row justify="center" align-content="center">
-      <div class="space"></div>
       <div>
+        <div class="space"></div>
         <v-progress-circular
           indeterminate
           :size="70"
