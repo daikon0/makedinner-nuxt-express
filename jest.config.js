7b6b1e31d7d71626a790fba2eba02d6930af4325
daikon0 <hamada1207.daiki@gmail.com>
2020-05-25 13:26:48 +0900

    add image mock

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,9 @@ module.exports = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
     '^~/(.*)$': '<rootDir>/$1',
-    '^vue$': 'vue/dist/vue.common.js'
+    '^vue$': 'vue/dist/vue.common.js',
+    '\\.(jpg|�|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/mocks/fileMock.js'
   },
   moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
