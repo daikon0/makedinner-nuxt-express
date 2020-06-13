ec139df0429f31b3cd72b0d96819d507b29982a9
daikon0 <hamada1207.daiki@gmail.com>
2020-06-13 12:01:57 +0900

    add btn for toHome

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,6 +5,9 @@
         <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
         <v-toolbar-title>{{ $store.state.title }}</v-toolbar-title>
+        <v-btn id="top" icon dark to="/mypage">
+          <v-icon large>mdi-home</v-icon>
+        </v-btn>
       </v-app-bar>
       <nuxt />
 
@@ -90,3 +93,9 @@ export default {
   }
 }
 </script>
+
+<style>
+#top {
+  margin-left: auto;
+}
+</style>
