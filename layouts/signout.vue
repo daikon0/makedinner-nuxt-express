a8ef827b1331cb1a9671cfb0f26e9a7d64b22176
daikon0 <hamada1207.daiki@gmail.com>
2020-06-13 21:44:41 +0900

    add home in layout

--- layouts/signout.vue
+++ layouts/signout.vue
@@ -5,6 +5,9 @@
         <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
         <v-toolbar-title>{{ $store.state.title }}</v-toolbar-title>
+        <v-btn id="top" icon dark to="/">
+          <v-icon large>mdi-home</v-icon>
+        </v-btn>
       </v-app-bar>
       <nuxt />
 
@@ -15,6 +18,13 @@
             active-class="deep-purple--text text--accent-4"
           >
             <v-list-item to="/">
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>ホーム</v-list-item-title>
+            </v-list-item>
+
+            <v-list-item to="/login">
               <v-list-item-icon>
                 <v-icon>mdi-account</v-icon>
               </v-list-item-icon>
