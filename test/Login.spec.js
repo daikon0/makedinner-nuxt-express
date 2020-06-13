bf36783a71c82d1a70e80b152d905fe0302d39e6
daikon0 <hamada1207.daiki@gmail.com>
2020-06-13 21:56:59 +0900

    fix indexTest

--- test/Login.spec.js
+++ test/Login.spec.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import Vue from 'vue'
 import cloneDeep from 'lodash.clonedeep'
-import IndexPage from '~/pages/index'
+import IndexPage from '~/pages/login'
 import store from '~/store/index'
 
 const localVue = createLocalVue()
@@ -11,7 +11,7 @@ Vue.use(Vuetify)
 localVue.use(Vuex)
 localVue.use(Vuetify)
 
-describe('pages/index.vue', () => {
+describe('pages/login.vue', () => {
   let wrapper
   let vuetify
 
