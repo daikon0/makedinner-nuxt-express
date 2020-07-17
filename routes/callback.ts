a6892ab7c297634df749dad3bf3851d6da787d6d
daikon0 <hamada1207.daiki@gmail.com>
2020-07-17 16:49:22 +0900

    add callback.ts

--- routes/callback.ts
+++ routes/callback.ts
@@ -1,10 +1,9 @@
 // const express = require('express')
-import express, { Response, NextFunction } from 'express'
-import { IGetUserAuthInfoRequest } from "./aaaa"
+import express, { Request, Response, NextFunction } from 'express'
 const router = express.Router()
 
 // storにuser情報を渡す
-router.get('/', (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
+router.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.json({ user: req.user })
 })
 
