67502684221979ae94aa5f4dc382f7790dc8a2a6
daikon0 <hamada1207.daiki@gmail.com>
2020-07-18 18:53:18 +0900

    fix commonjs

--- dist/callback.js
+++ dist/callback.js
@@ -4,10 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // const express = require('express')
-var express_1 = __importDefault(require("express"));
-var router = express_1.default.Router();
+const express_1 = __importDefault(require("express"));
+const router = express_1.default.Router();
 // storにuser情報を渡す
-router.get('/', function (req, res, next) {
+router.get('/', (req, res, next) => {
     res.json({ user: req.user });
 });
 module.exports = router;
