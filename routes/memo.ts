aa390f6314583602f8860b51dd9fe1d282e3fe3e
daikon0 <hamada1207.daiki@gmail.com>
2020-07-19 22:05:34 +0900

    add ts

--- routes/memo.ts
+++ routes/memo.ts
@@ -1,24 +1,24 @@
-const express = require('express')
-const router = express.Router()
+import express, { Request, Response, NextFunction } from 'express'
+import csrf from 'csurf'
 
-const csrf = require('csurf')
+const router = express.Router()
 const csrfProtection = csrf({ cookie: true })
 
 const db = require('../models/index')
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req: any, res: Response, next: NextFunction) => {
   const memos = await db.memo
     .findAll({
       where: { createdBy: req.user.name.id },
       order: [['"createdAt"', 'DESC']]
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       next(err)
     })
   res.send(memos)
 })
 
-router.post('/', csrfProtection, async (req, res, next) => {
+router.post('/', csrfProtection, async (req: any, res: Response, next: NextFunction) => {
   await db.memo
     .create({
       name: req.body.name,
@@ -28,58 +28,58 @@ router.post('/', csrfProtection, async (req, res, next) => {
     .then(() => {
       res.redirect('/mypage/memo')
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       next(err)
     })
 })
 
-router.post('/edit', csrfProtection, async (req, res, next) => {
+router.post('/edit', csrfProtection, async (req: any, res: Response, next: NextFunction) => {
   const memo = await db.memo
     .findOne({
       where: { id: req.body.memoId }
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       next(err)
     })
   memo
     .update({
       done: !memo.done
     })
-    .catch((err) => {
+    .catch((err: Error) => {
       next(err)
     })
   res.redirect('/mypage/memo')
 })
 
-router.post('/delete', csrfProtection, async (req, res, next) => {
-  await deleteMemo(req.body.memoId).catch((err) => {
+router.post('/delete', csrfProtection, async (req: any, res: Response, next: NextFunction) => {
+  await deleteMemo(req.body.memoId).catch((err: Error) => {
     next(err)
   })
   res.redirect('/mypage/memo')
 })
 
-router.post('/deleteAll', csrfProtection, async (req, res, next) => {
-  await deleteAll(req.user.name.id).catch((err) => {
+router.post('/deleteAll', csrfProtection, async (req: any, res: Response, next: NextFunction) => {
+  await deleteAll(req.user.name.id).catch((err: Error) => {
     next(err)
   })
   res.redirect('/mypage/memo')
 })
 
-async function deleteMemo(id, done, err) {
+async function deleteMemo(id: bigint) {
   const memo = await db.memo.findAll({
     where: { id }
   })
-  const promises = memo.map((d) => {
+  const promises = memo.map((d: any) => {
     return d.destroy()
   })
   return Promise.all(promises)
 }
 
-async function deleteAll(createdBy, done, err) {
+async function deleteAll(createdBy: bigint) {
   const memo = await db.memo.findAll({
     where: { createdBy }
   })
-  const promises = memo.map((d) => {
+  const promises = memo.map((d: any) => {
     return d.destroy()
   })
   return Promise.all(promises)
