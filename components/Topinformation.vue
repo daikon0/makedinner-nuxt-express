665f82f265898f488b07808f3636c2d8028e82b4
daikon0 <hamada1207.daiki@gmail.com>
2020-07-31 01:56:03 +0900

    fix responsive

--- components/Topinformation.vue
+++ components/Topinformation.vue
@@ -2,19 +2,25 @@
   <v-col class="mx-6" cols="12" md="5" sm="8">
     <v-card class="mt-5" elevation="5">
       <v-list-item three-line>
-        <v-list-item-content>
-          <p class="title my-1 text-justify">
-            {{ title }}
-          </p>
-          <p class="text--secondary">
-            {{ subtitle }}
-            <br />
-            {{ undersubtitle }}
-          </p>
-        </v-list-item-content>
-        <v-list-item-avatar tile size="180" color="grey">
-          <img :src="require(`@/assets/${image}`)" />
-        </v-list-item-avatar>
+        <div>
+          <v-list-item-content>
+            <v-col cols="12" md="5" sm="12">
+              <img class="image" :src="require(`@/assets/${image}`)" />
+            </v-col>
+            <v-col cols="12" md="7" sm="12">
+              <div class="text-center">
+                <p class="title mt-3" md="10">
+                  {{ title }}
+                </p>
+                <p class="text--secondary text-justify">
+                  {{ subtitle }}
+                  <br />
+                  {{ undersubtitle }}
+                </p>
+              </div>
+            </v-col>
+          </v-list-item-content>
+        </div>
       </v-list-item>
     </v-card>
   </v-col>
@@ -42,3 +48,11 @@ export default {
   }
 }
 </script>
+
+<style>
+.image {
+  width: 100%;
+  max-width: 100%;
+  height: auto;
+}
+</style>
